7b035e920be5863915949705d6df922de859bad8
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-08 17:45:39 +0100

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const squares = document.querySelectorAll('.square');
 const balanceDisplay = document.getElementById('balance');
-const restartButton = document.getElementById('restart');
+const restartButton = document.getElementById('restart-button');
+
 let balance = 0;
 
 squares.forEach((square) => {
@@ -14,7 +15,7 @@ squares.forEach((square) => {
             balance += 30;
             balanceDisplay.textContent = balance;
             square.style.backgroundColor = 'transparent';
-            square.textContent = '💣'; // Bomb emoji
+            square.textContent = '\u{1F4B0}'; // Money bag emoji
             square.removeEventListener('click', () => {});
         }
     });
@@ -28,6 +29,7 @@ function restartGame() {
     squares.forEach((square) => {
         square.style.backgroundColor = 'black';
         square.textContent = '';
+        square.setAttribute('data-is-bomb', (Math.random() < 0.3).toString());
         square.addEventListener('click', () => {
             if (square.getAttribute('data-is-bomb') === 'true') {
                 alert('Game Over! You hit a bomb. Your balance will be reset.');
@@ -38,7 +40,7 @@ function restartGame() {
                 balance += 30;
                 balanceDisplay.textContent = balance;
                 square.style.backgroundColor = 'transparent';
-                square.textContent = '💣'; // Bomb emoji
+                square.textContent = '\u{1F4B0}'; // Money bag emoji
                 square.removeEventListener('click', () => {});
             }
         });
