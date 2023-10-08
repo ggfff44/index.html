a50e00956a9e49e3a66443412b1d7198cdbe229c
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-08 17:59:26 +0100

    script.js

--- script.js
+++ script.js
@@ -1,70 +1,41 @@
-const gameBoard = document.getElementById('game-board');
-const restartButton = document.getElementById('restart-button');
+const board = document.querySelector('.board');
 const balanceDisplay = document.getElementById('balance');
+const restartButton = document.getElementById('restart-button');
 
-let cards = [];
 let balance = 0;
 
-// Function to create a shuffled deck
-function createDeck() {
-    const deck = [];
-    for (let i = 0; i < 3; i++) {
-        deck.push('robux', 'bomb', 'bomb');
-    }
-    return shuffle(deck);
-}
-
-// Function to shuffle an array
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
-
-// Function to handle card click
-function handleCardClick(index) {
-    const card = cards[index];
-
-    if (card.classList.contains('hidden')) {
-        card.classList.remove('hidden');
-
-        if (card.dataset.value === 'robux') {
-            balance += 30;
-        } else {
-            balance -= 80;
+function createSquare(isBomb) {
+    const square = document.createElement('div');
+    square.className = 'square';
+    square.textContent = isBomb ? '💣' : '💰';
+    square.addEventListener('click', () => {
+        if (isBomb) {
+            alert('Game Over! You hit a bomb. Your balance will be reset.');
+            balance = 0;
+            balanceDisplay.textContent = balance;
             restartGame();
+        } else {
+            balance += 30;
+            balanceDisplay.textContent = balance;
+            square.style.backgroundColor = 'transparent';
+            square.style.cursor = 'default';
+            square.removeEventListener('click', () => { });
         }
+    });
+    return square;
+}
 
-        balanceDisplay.textContent = balance + ' Robux';
+function restartGame() {
+    board.innerHTML = '';
+    for (let i = 0; i < 9; i++) {
+        const isBomb = Math.random() < 0.33; // Approximately 33% of squares will be bombs
+        const square = createSquare(isBomb);
+        board.appendChild(square);
     }
 }
 
-// Function to restart the game
-function restartGame() {
-    cards.forEach((card) => {
-        card.classList.add('hidden');
-    });
-
-    cards = createDeck();
-
-    setTimeout(() => {
-        cards.forEach((card, index) => {
-            card.classList.add('hidden');
-            card.dataset.value = cards[index];
-        });
-    }, 2000);
-}
-
-// Initialize the game
-restartGame();
-
-// Add click event listeners to the cards
-cards.forEach((card, index) => {
-    card.addEventListener('click', () => handleCardClick(index));
-    gameBoard.appendChild(card);
+restartButton.addEventListener('click', () => {
+    restartGame();
 });
 
-// Add click event listener to the restart button
-restartButton.addEventListener('click', restartGame);
+restartGame();
