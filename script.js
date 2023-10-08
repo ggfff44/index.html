9d45669aada9d4489c933be23a4bbec1d0cd2715
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-08 16:37:01 +0100

    script.js

--- script.js
+++ script.js
@@ -1,83 +1,109 @@
-const gameBoard = document.getElementById('gameBoard');
-const robuxBalance = document.getElementById('robux');
-const withdrawButton = document.getElementById('withdraw');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memory Card Game</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: grey;
+        }
+        .card {
+            width: 100px;
+            height: 100px;
+            background-color: black;
+            margin: 5px;
+            display: inline-block;
+            cursor: pointer;
+        }
+        .card img {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <div id="game-board">
+    </div>
+    <script>
+        const cards = [
+            { id: 1, imgSrc: 'robux.png' },
+            { id: 2, imgSrc: 'robux.png' },
+            { id: 3, imgSrc: 'robux.png' },
+            { id: 4, imgSrc: 'robux.png' },
+            { id: 5, imgSrc: 'robux.png' },
+            { id: 6, imgSrc: 'robux.png' },
+            { id: 7, imgSrc: 'robux.png' },
+            { id: 8, imgSrc: 'robux.png' },
+            { id: 9, imgSrc: 'bomb.png' }
+        ];
 
-const boardSize = 5;
-const numBombs = Math.floor((boardSize * boardSize) * 0.5);
-const robuxPerGame = 10;
-const robuxPerWin = 300;
+        let openedCards = [];
+        let matchedCards = [];
 
-let robux = 100;
+        function shuffle(array) {
+            let currentIndex = array.length, randomIndex;
 
-const generateGameBoard = () => {
-    for (let i = 0; i < boardSize * boardSize; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        gameBoard.appendChild(square);
+            while (currentIndex !== 0) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex--;
 
-        square.addEventListener('click', () => {
-            if (square.classList.contains('flag') || square.classList.contains('bomb')) {
-                return;
+                [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
             }
 
-            if (Math.random() < 0.5) {
-                square.classList.add('flag');
-                robux += robuxPerWin;
-            } else {
-                square.classList.add('bomb');
-                robux = 0;
-                alert('You touched a bomb! Game over.');
-                resetGame();
-            }
+            return array;
+        }
 
-            updateRobux();
-        });
-    }
-};
+        function createGameBoard() {
+            const gameBoard = document.getElementById('game-board');
+            const shuffledCards = shuffle(cards);
 
-const updateRobux = () => {
-    robuxBalance.textContent = robux;
-    if (robux >= 10000) {
-        withdrawButton.disabled = false;
-    } else {
-        withdrawButton.disabled = true;
-    }
-};
+            shuffledCards.forEach(card => {
+                const cardElement = document.createElement('div');
+                cardElement.classList.add('card');
+                cardElement.dataset.id = card.id;
 
-const resetGame = () => {
-    gameBoard.innerHTML = '';
-    generateGameBoard();
-    updateRobux();
-};
+                const cardImage = document.createElement('img');
+                cardImage.src = card.imgSrc;
+                cardImage.alt = 'Card';
+                cardElement.appendChild(cardImage);
 
-withdrawButton.addEventListener('click', () => {
-    if (robux >= 10000) {
-        robux -= 10000;
-        alert('Withdraw successful! You now have 0 robux.');
-        updateRobux();
-    } else {
-        alert('You need at least 10,000 robux to withdraw.');
-    }
-});
+                cardElement.addEventListener('click', () => {
+                    if (!cardElement.classList.contains('opened') && openedCards.length < 2) {
+                        cardElement.classList.add('opened');
+                        cardImage.style.display = 'block';
+                        openedCards.push(cardElement);
 
-generateGameBoard();
-updateRobux();
+                        if (openedCards.length === 2) {
+                            const [card1, card2] = openedCards;
 
-// Function to end the game
-function endGame(isWinner) {
-    isGameActive = false;
-    if (!isWinner) {
-        setTimeout(() => {
-            const cells = document.querySelectorAll(".cell");
-            cells.forEach((cell) => {
-                if (cell.classList.contains("bomb-cell")) {
-                    cell.classList.remove("flipped");
-                }
-                cell.removeEventListener("click", () => clickCell(cell));
+                            if (card1.dataset.id === card2.dataset.id) {
+                                matchedCards.push(card1, card2);
+                                openedCards = [];
+
+                                if (matchedCards.length === cards.length) {
+                                    setTimeout(() => {
+                                        alert('Congratulations! You won the game!');
+                                    }, 500);
+                                }
+                            } else {
+                                setTimeout(() => {
+                                    card1.classList.remove('opened');
+                                    card2.classList.remove('opened');
+                                    card1.querySelector('img').style.display = 'none';
+                                    card2.querySelector('img').style.display = 'none';
+                                    openedCards = [];
+                                }, 1000);
+                            }
+                        }
+                    }
+                });
+
+                gameBoard.appendChild(cardElement);
             });
-            createBoard();
-            isGameActive = true;
-        }, 1000);
-    }
-}
+        }
 
+        createGameBoard();
+    </script>
+</body>
+</html>
