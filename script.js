aa14aed49dd576338bb60330f171150e4248da9c
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-08 18:34:45 +0100

    script.js

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ const balanceDisplay = document.getElementById('balance');
 const restartButton = document.getElementById('restart');
 let balance = 0;
 
-squares.forEach((square) => {
+// Create an array to represent the board with 3 bombs and 6 good boxes
+const board = Array(9).fill('good');
+board.fill('bomb', 0, 3);
+shuffleArray(board);
+
+squares.forEach((square, index) => {
     square.addEventListener('click', () => {
-        if (square.getAttribute('data-is-bomb') === 'true') {
+        if (square.classList.contains('bomb')) {
             alert('Game Over! You hit a bomb. Your balance will be reset.');
             balance = 0;
             balanceDisplay.textContent = balance;
@@ -13,11 +18,29 @@ squares.forEach((square) => {
         } else {
             balance += 30;
             balanceDisplay.textContent = balance;
-            square.style.backgroundColor = 'transparent';
+            square.classList.remove('cover');
             square.textContent = '💰';
             square.removeEventListener('click', () => {});
+            // Check if all good boxes have been clicked
+            const allGoodBoxesClicked = [...squares].filter((s) => s.classList.contains('good') && !s.classList.contains('cover')).length === 0;
+            if (allGoodBoxesClicked) {
+                alert('Congratulations! You won. Restarting the game.');
+                restartGame();
+            }
         }
     });
+
+    // Add a cover to each square
+    const cover = document.createElement('div');
+    cover.classList.add('cover');
+    square.appendChild(cover);
+
+    // Set the initial state for each square
+    if (board[index] === 'bomb') {
+        square.classList.add('bomb', 'cover');
+    } else if (board[index] === 'good') {
+        square.classList.add('good', 'cover');
+    }
 });
 
 restartButton.addEventListener('click', () => {
@@ -25,22 +48,21 @@ restartButton.addEventListener('click', () => {
 });
 
 function restartGame() {
-    squares.forEach((square) => {
-        square.style.backgroundColor = 'black';
+    board.fill('good', 0, 6);
+    board.fill('bomb', 6, 9);
+    shuffleArray(board);
+
+    squares.forEach((square, index) => {
+        square.classList.remove('good', 'bomb');
+        square.classList.add('cover');
         square.textContent = '';
-        square.addEventListener('click', () => {
-            if (square.getAttribute('data-is-bomb') === 'true') {
-                alert('Game Over! You hit a bomb. Your balance will be reset.');
-                balance = 0;
-                balanceDisplay.textContent = balance;
-                restartGame();
-            } else {
-                balance += 30;
-                balanceDisplay.textContent = balance;
-                square.style.backgroundColor = 'transparent';
-                square.textContent = '💰';
-                square.removeEventListener('click', () => {});
-            }
-        });
     });
+}
+
+// Function to shuffle an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
