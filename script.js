c9ee615676c908011351a2b88f6917271ef6ae5f
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-08 17:10:36 +0100

    script.js

--- script.js
+++ script.js
@@ -29,15 +29,8 @@ function createCard(imgSrc) {
 }
 
 function flipCard(card) {
-  card.classList.add("flipped");
-}
-
-function unflipCards() {
-  flippedCards.forEach((card) => {
-    card.classList.remove("flipped");
-  });
-  flippedCards = [];
-  canFlip = true;
+  const img = card.querySelector("img");
+  img.style.display = "block";
 }
 
 function checkMatch() {
@@ -47,48 +40,47 @@ function checkMatch() {
 
   if (img1 === img2) {
     robuxCount += 100;
-    document.getElementById("robux-count").textContent = `${robuxCount} Robux`;
+    document.getElementById("robux-count").textContent = `Robux: ${robuxCount}`;
     flippedCards = [];
+    canFlip = true;
   } else {
-    unflipCards();
+    setTimeout(() => {
+      flipCard(card1);
+      flipCard(card2);
+      flippedCards = [];
+      canFlip = true;
+    }, 500);
   }
 }
 
-function initializeGame() {
-  const gameBoard = document.getElementById("game-board");
+function startGame() {
+  const gameContainer = document.getElementById("game-container");
+  gameContainer.innerHTML = "";
 
-  // Replace with your image URLs, ensuring you have both Robux and Bomb images.
-  const imageUrls = [
-    "https://media.printables.com/media/prints/128836/images/1234294_ba6edb95-e18f-4feb-a7c1-614c16f4c603/thumbs/inside/1280x960/png/robuxcoin.webp",
-    "https://www.dictionary.com/e/wp-content/uploads/2018/07/bomb-emoji.png",
+  const cardImages = [
+    "robuxcoin.webp",
+    "bomb-emoji.png",
+    "robuxcoin.webp",
+    "bomb-emoji.png",
+    "robuxcoin.webp",
+    "bomb-emoji.png",
   ];
 
-  // Create 9 cards with 4 pairs of images
-  for (let i = 0; i < 4; i++) {
-    imageUrls.forEach((imgSrc) => {
-      const card = createCard(imgSrc);
-      cards.push(card);
-    });
+  shuffleArray(cardImages);
+
+  for (const imgSrc of cardImages) {
+    const card = createCard(imgSrc);
+    gameContainer.appendChild(card);
   }
 
-  // Shuffle the cards
-  cards.sort(() => Math.random() - 0.5);
-
-  // Add cards to the game board
-  cards.forEach((card) => {
-    gameBoard.appendChild(card);
-  });
+  document.querySelector(".cover").style.display = "none";
 }
 
-const cashOutButton = document.getElementById("cash-out");
-cashOutButton.addEventListener("click", () => {
-  if (robuxCount >= 25000) {
-    robuxCount -= 25000;
-    alert("Cash out successful! You now have 25000 Robux.");
-    document.getElementById("robux-count").textContent = `${robuxCount} Robux`;
-  } else {
-    alert("You need at least 25000 Robux to cash out.");
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-});
+}
 
-initializeGame();
+document.querySelector(".cover").addEventListener("click", startGame);
