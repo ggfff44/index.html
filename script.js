419484de986521c5b5b8fd3c5ab9775da3459de5
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-08 17:21:31 +0100

    script.js

--- script.js
+++ script.js
@@ -1,61 +1,83 @@
+const balanceDisplay = document.getElementById('balanceDisplay');
 const board = document.getElementById('board');
-const balanceDisplay = document.getElementById('balance');
-const restartButton = document.getElementById('restart-button');
-
+const restartButton = document.getElementById('restartButton');
 let balance = 0;
 let isGameOver = false;
 
-const images = [
-    'https://media.printables.com/media/prints/128836/images/1234294_ba6edb95-e18f-4feb-a7c1-614c16f4c603/thumbs/inside/1280x960/png/robuxcoin.webp',
-    'https://www.dictionary.com/e/wp-content/uploads/2018/07/bomb-emoji.png',
-];
+// Shuffle array elements randomly
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
-const shuffledImages = [...images, ...images].sort(() => Math.random() - 0.5);
+// Create an array of indexes for bomb squares
+const bombIndexes = Array.from({ length: 3 }, (_, index) => index);
+shuffleArray(bombIndexes);
 
 // Create a 3x3 grid of squares
 for (let i = 0; i < 9; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     square.dataset.index = i;
-    square.style.backgroundImage = `url(${shuffledImages[i]})`;
-    board.appendChild(square);
-}
 
-// Add click event to each square
-const squares = document.querySelectorAll('.square');
-squares.forEach((square) => {
+    // Check if this square is a bomb
+    const isBomb = bombIndexes.includes(i);
+    if (isBomb) {
+        square.dataset.isBomb = 'true';
+    }
+
     square.addEventListener('click', () => {
         if (isGameOver) return;
         const index = square.dataset.index;
-        const clickedImage = shuffledImages[index];
-        if (clickedImage === 'https://media.printables.com/media/prints/128836/images/1234294_ba6edb95-e18f-4feb-a7c1-614c16f4c603/thumbs/inside/1280x960/png/robuxcoin.webp') {
-            balance += 100;
+        const isBomb = square.dataset.isBomb === 'true';
+
+        // Reveal square content
+        square.style.backgroundImage = `url(${isBomb ? 'https://www.dictionary.com/e/wp-content/uploads/2018/07/bomb-emoji.png' : 'https://media.printables.com/media/prints/128836/images/1234294_ba6edb95-e18f-4feb-a7c1-614c16f4c603/thumbs/inside/1280x960/png/robuxcoin.webp'})`;
+        square.style.pointerEvents = 'none'; // Disable clicking on matched square
+
+        if (!isBomb) {
+            balance += 30; // +30 robux for a right guess
             balanceDisplay.textContent = `Robux: ${balance}`;
-            square.style.pointerEvents = 'none'; // Disable clicking on matched square
-        } else if (clickedImage === 'https://www.dictionary.com/e/wp-content/uploads/2018/07/bomb-emoji.png') {
-            square.style.backgroundImage = 'url("https://www.dictionary.com/e/wp-content/uploads/2018/07/bomb-emoji.png")'; // Show bomb image
+        } else {
             isGameOver = true;
+            balanceDisplay.textContent = 'Game Over';
         }
         checkWin();
     });
+
+    board.appendChild(square);
+}
+
+restartButton.addEventListener('click', () => {
+    restartGame();
 });
 
-// Check if the game is won
+function restartGame() {
+    isGameOver = false;
+    balance = 0;
+    balanceDisplay.textContent = 'Robux: 0';
+
+    // Reset the board
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.style.backgroundImage = 'none';
+        square.style.pointerEvents = 'auto';
+    });
+
+    // Reshuffle bomb positions
+    shuffleArray(bombIndexes);
+    squares.forEach((square, index) => {
+        const isBomb = bombIndexes.includes(index);
+        square.dataset.isBomb = isBomb ? 'true' : 'false';
+    });
+}
+
 function checkWin() {
-    if (document.querySelectorAll('.square[style*="background-image: url(\\"https://media.printables.com/media/prints/128836/images/1234294_ba6edb95-e18f-4feb-a7c1-614c16f4c603/thumbs/inside/1280x960/png/robuxcoin.webp\\")"]').length === 9) {
-        balanceDisplay.textContent = 'You Win!';
+    const revealedSquares = document.querySelectorAll('.square:not([style*="none"])');
+    if (revealedSquares.length === 6) {
+        balanceDisplay.textContent = 'You Win! Robux: ' + balance;
         isGameOver = true;
     }
 }
-
-// Restart the game
-restartButton.addEventListener('click', () => {
-    balance = 0;
-    balanceDisplay.textContent = `Robux: ${balance}`;
-    isGameOver = false;
-    squares.forEach((square) => {
-        square.style.backgroundImage = '';
-        square.style.pointerEvents = 'auto';
-    });
-    shuffledImages.sort(() => Math.random() - 0.5);
-});
