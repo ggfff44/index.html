7422cd60e413d7d0bf994fa3a551be98ab438b39
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-08 16:39:18 +0100

    script.js

--- script.js
+++ script.js
@@ -1,109 +1,96 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Memory Card Game</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: grey;
+const cards = [
+    { id: 0, imgSrc: 'robux.png', isFlipped: false },
+    { id: 1, imgSrc: 'robux.png', isFlipped: false },
+    { id: 2, imgSrc: 'robux.png', isFlipped: false },
+    { id: 3, imgSrc: 'robux.png', isFlipped: false },
+    { id: 4, imgSrc: 'robux.png', isFlipped: false },
+    { id: 5, imgSrc: 'robux.png', isFlipped: false },
+    { id: 6, imgSrc: 'robux.png', isFlipped: false },
+    { id: 7, imgSrc: 'robux.png', isFlipped: false },
+    { id: 8, imgSrc: 'robux.png', isFlipped: false }
+];
+
+let firstCard = null;
+let secondCard = null;
+let matches = 0;
+let attempts = 0;
+let robux = 0;
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function flipCard(index) {
+    if (cards[index].isFlipped) return;
+    const card = document.querySelector(`.card:nth-child(${index + 1})`);
+    card.innerHTML = `<img src="${cards[index].imgSrc}" alt="Card">`;
+    cards[index].isFlipped = true;
+
+    if (firstCard === null) {
+        firstCard = index;
+    } else {
+        secondCard = index;
+        attempts++;
+        document.getElementById('attempts').textContent = `Attempts: ${attempts}`;
+        setTimeout(checkMatch, 1000);
+    }
+}
+
+function checkMatch() {
+    if (cards[firstCard].imgSrc === cards[secondCard].imgSrc) {
+        matches++;
+        robux += 100; // Reward 100 robux for a match
+        document.getElementById('robux').textContent = `Robux: ${robux}`;
+
+        if (matches === cards.length / 2) {
+            alert('Congratulations! You won!');
+            resetGame();
+        } else {
+            firstCard = null;
+            secondCard = null;
         }
-        .card {
-            width: 100px;
-            height: 100px;
-            background-color: black;
-            margin: 5px;
-            display: inline-block;
-            cursor: pointer;
+    } else {
+        const card1 = document.querySelector(`.card:nth-child(${firstCard + 1})`);
+        const card2 = document.querySelector(`.card:nth-child(${secondCard + 1})`);
+        card1.innerHTML = '';
+        card2.innerHTML = '';
+        cards[firstCard].isFlipped = false;
+        cards[secondCard].isFlipped = false;
+        firstCard = null;
+        secondCard = null;
+
+        // Penalize 50 robux for a mismatch
+        robux -= 50;
+        document.getElementById('robux').textContent = `Robux: ${robux}`;
+
+        if (robux < 0) {
+            alert('You ran out of Robux! Game over.');
+            resetGame();
         }
-        .card img {
-            display: none;
-        }
-    </style>
-</head>
-<body>
-    <div id="game-board">
-    </div>
-    <script>
-        const cards = [
-            { id: 1, imgSrc: 'robux.png' },
-            { id: 2, imgSrc: 'robux.png' },
-            { id: 3, imgSrc: 'robux.png' },
-            { id: 4, imgSrc: 'robux.png' },
-            { id: 5, imgSrc: 'robux.png' },
-            { id: 6, imgSrc: 'robux.png' },
-            { id: 7, imgSrc: 'robux.png' },
-            { id: 8, imgSrc: 'robux.png' },
-            { id: 9, imgSrc: 'bomb.png' }
-        ];
+    }
+}
 
-        let openedCards = [];
-        let matchedCards = [];
+function resetGame() {
+    matches = 0;
+    attempts = 0;
+    robux = 0;
+    document.getElementById('attempts').textContent = 'Attempts: 0';
+    document.getElementById('robux').textContent = 'Robux: 0';
 
-        function shuffle(array) {
-            let currentIndex = array.length, randomIndex;
+    cards.forEach(card => {
+        card.isFlipped = false;
+    });
 
-            while (currentIndex !== 0) {
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex--;
+    shuffleArray(cards);
 
-                [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-            }
+    const cardElements = document.querySelectorAll('.card');
+    cardElements.forEach((card, index) => {
+        card.innerHTML = '';
+        card.style.pointerEvents = 'auto';
+    });
+}
 
-            return array;
-        }
-
-        function createGameBoard() {
-            const gameBoard = document.getElementById('game-board');
-            const shuffledCards = shuffle(cards);
-
-            shuffledCards.forEach(card => {
-                const cardElement = document.createElement('div');
-                cardElement.classList.add('card');
-                cardElement.dataset.id = card.id;
-
-                const cardImage = document.createElement('img');
-                cardImage.src = card.imgSrc;
-                cardImage.alt = 'Card';
-                cardElement.appendChild(cardImage);
-
-                cardElement.addEventListener('click', () => {
-                    if (!cardElement.classList.contains('opened') && openedCards.length < 2) {
-                        cardElement.classList.add('opened');
-                        cardImage.style.display = 'block';
-                        openedCards.push(cardElement);
-
-                        if (openedCards.length === 2) {
-                            const [card1, card2] = openedCards;
-
-                            if (card1.dataset.id === card2.dataset.id) {
-                                matchedCards.push(card1, card2);
-                                openedCards = [];
-
-                                if (matchedCards.length === cards.length) {
-                                    setTimeout(() => {
-                                        alert('Congratulations! You won the game!');
-                                    }, 500);
-                                }
-                            } else {
-                                setTimeout(() => {
-                                    card1.classList.remove('opened');
-                                    card2.classList.remove('opened');
-                                    card1.querySelector('img').style.display = 'none';
-                                    card2.querySelector('img').style.display = 'none';
-                                    openedCards = [];
-                                }, 1000);
-                            }
-                        }
-                    }
-                });
-
-                gameBoard.appendChild(cardElement);
-            });
-        }
-
-        createGameBoard();
-    </script>
-</body>
-</html>
+shuffleArray(cards);
