95d9336ca768b24dd4bc7691f8cc2d0b50a550bb
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-09 01:48:20 +0100

    script.js

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-const board = document.getElementById('game-board');
+const squares = document.querySelectorAll('.square');
 const balanceDisplay = document.getElementById('balance');
 const restartButton = document.getElementById('restart');
 let balance = 0;
 
-// Function to create the game board
-function createBoard() {
-    for (let i = 0; i < 9; i++) {
-        const square = document.createElement('div');
-        square.className = 'square';
-        square.textContent = '?';
-        square.dataset.isBomb = Math.random() < 0.33; // Approximately 1/3 chance of being a bomb
-        board.appendChild(square);
-
-        square.addEventListener('click', () => {
-            if (square.textContent === '?') {
-                if (square.dataset.isBomb === 'true') {
-                    square.textContent = '💣';
-                    alert('Game Over! You hit a bomb. Your balance will be reset.');
-                    balance = 0;
-                    balanceDisplay.textContent = balance;
-                    restartGame();
-                } else {
-                    square.textContent = '💰';
-                    balance += 30;
-                    balanceDisplay.textContent = balance;
-                }
-            }
-        });
-    }
-}
-
-// Function to restart the game
-function restartGame() {
-    board.innerHTML = '';
-    createBoard();
-}
-
-createBoard();
+squares.forEach((square) => {
+    square.addEventListener('click', () => {
+        if (square.getAttribute('data-is-bomb') === 'true') {
+            alert('Game Over! You hit a bomb. Your balance will be reset.');
+            balance = 0;
+            balanceDisplay.textContent = balance;
+            restartGame();
+        } else {
+            balance += 30;
+            balanceDisplay.textContent = balance;
+            square.style.backgroundColor = 'transparent';
+            square.textContent = '';
+            square.removeEventListener('click', () => {});
+        }
+    });
+});
 
 restartButton.addEventListener('click', () => {
     restartGame();
 });
+
+function restartGame() {
+    squares.forEach((square) => {
+        square.style.backgroundColor = 'black';
+        square.textContent = '';
+        square.addEventListener('click', () => {
+            if (square.getAttribute('data-is-bomb') === 'true') {
+                alert('Game Over! You hit a bomb. Your balance will be reset.');
+                balance = 0;
+                balanceDisplay.textContent = balance;
+                restartGame();
+            } else {
+                balance += 30;
+                balanceDisplay.textContent = balance;
+                square.style.backgroundColor = 'transparent';
+                square.textContent = '';
+                square.removeEventListener('click', () => {});
+            }
+        });
+    });
+}
+
