8f8eb758a9c73436f3397b38fe7ef60da6daaf5f
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-09 02:08:15 +0100

    script.js

--- script.js
+++ script.js
@@ -1,47 +1,55 @@
-const squares = document.querySelectorAll('.square');
-const balanceDisplay = document.getElementById('balance');
-const restartButton = document.getElementById('restart');
-let balance = 0;
+let robux = 100;
+let betSize = 0;
 
-squares.forEach((square) => {
-    square.addEventListener('click', () => {
-        if (square.getAttribute('data-is-bomb') === 'true') {
-            alert('Game Over! You hit a bomb. Your balance will be reset.');
-            balance = 0;
-            balanceDisplay.textContent = balance;
-            restartGame();
-        } else {
-            balance += 30;
-            balanceDisplay.textContent = balance;
-            square.style.backgroundColor = 'transparent';
-            square.textContent = '';
-            square.removeEventListener('click', () => {});
+function startGame() {
+    // Get the selected bet size
+    const betRadio = document.querySelector('input[name="bet"]:checked');
+    betSize = parseFloat(betRadio.value);
+
+    if (robux >= betSize) {
+        robux -= betSize;
+        updateBalance();
+
+        const board = document.getElementById('board');
+        board.innerHTML = '';
+        
+        for (let i = 0; i < 9; i++) {
+            const square = document.createElement('div');
+            square.className = 'square';
+            square.addEventListener('click', revealSquare);
+            board.appendChild(square);
         }
-    });
-});
-
-restartButton.addEventListener('click', () => {
-    restartGame();
-});
-
-function restartGame() {
-    squares.forEach((square) => {
-        square.style.backgroundColor = 'black';
-        square.textContent = '';
-        square.addEventListener('click', () => {
-            if (square.getAttribute('data-is-bomb') === 'true') {
-                alert('Game Over! You hit a bomb. Your balance will be reset.');
-                balance = 0;
-                balanceDisplay.textContent = balance;
-                restartGame();
-            } else {
-                balance += 30;
-                balanceDisplay.textContent = balance;
-                square.style.backgroundColor = 'transparent';
-                square.textContent = '';
-                square.removeEventListener('click', () => {});
-            }
-        });
-    });
+    } else {
+        alert("Not enough Robux to bet!");
+    }
 }
 
+function revealSquare(event) {
+    const square = event.target;
+    const randomNumber = Math.random();
+
+    if (randomNumber < 0.3) {
+        square.textContent = '🔥'; // Bomb
+        endGame();
+    } else {
+        square.textContent = '💰'; // Point
+        robux += betSize * 2;
+        updateBalance();
+    }
+
+    square.style.backgroundColor = 'transparent';
+    square.style.border = 'none';
+}
+
+function endGame() {
+    setTimeout(() => {
+        alert('Game over! You hit a bomb.');
+        robux -= betSize;
+        updateBalance();
+        startGame();
+    }, 1000);
+}
+
+function updateBalance() {
+    document.getElementById('balance').textContent = `Robux: ${robux}`;
+}
