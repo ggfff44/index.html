ebfa2488bacc553213695974d58cae03e848dc0a
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-09 23:56:21 +0100

    script.js

--- script.js
+++ script.js
@@ -2,23 +2,52 @@
 function startGame(betSize) {
     // Reset the game board
     document.getElementById("gameBoard").innerHTML = "";
-
+    
     // Create a new game board with 3x3 squares
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             const square = document.createElement("div");
             square.className = "square";
-            square.onclick = () => revealSquare(square);
+            square.setAttribute("data-value", ""); // Add a data attribute to store square value
+            square.onclick = () => revealSquare(square, betSize);
             document.getElementById("gameBoard").appendChild(square);
         }
     }
-
+    
     // Handle bets and game logic here
     const robuxBalance = document.getElementById("robuxBalance");
     robuxBalance.textContent = parseFloat(robuxBalance.textContent) - (betSize * 100);
 }
 
-function revealSquare(square) {
+function revealSquare(square, betSize) {
+    // Check if the square has already been revealed
+    if (square.getAttribute("data-value") !== "") {
+        return;
+    }
+
     // Implement logic to reveal squares and check for bombs or points
-    // You can use emojis for bombs and points, and update the player's balance
+    // You can use emojis for bombs (🔥) and points (💰)
+    const randomValue = Math.random();
+    if (randomValue < 0.33) {
+        square.textContent = "🔥"; // Bomb
+    } else {
+        square.textContent = "💰"; // Point
+    }
+
+    // Update the data-value attribute to indicate the square has been revealed
+    square.setAttribute("data-value", square.textContent);
+
+    // Check if the player has lost and end the game
+    if (square.textContent === "🔥") {
+        alert("You hit a bomb! Game over.");
+        document.getElementById("gameBoard").innerHTML = ""; // Clear the game board
+    } else {
+        // Check if all squares have been revealed (win condition)
+        const allSquares = document.querySelectorAll(".square");
+        const allRevealed = Array.from(allSquares).every(square => square.getAttribute("data-value") !== "");
+        if (allRevealed) {
+            alert("Congratulations! You won.");
+            // You can add logic here to give the player a reward based on their betSize
+        }
+    }
 }
