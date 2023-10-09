e78c6ed6b09e1a63ae38e361d82ae219bfad79ae
ggfff44 <147286203+ggfff44@users.noreply.github.com>
2023-10-09 02:14:55 +0100

    script.js

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
-// Define game variables
 let robux = 100;
 let isGameStarted = false;
 
-// Function to start the game
 function startGame() {
     if (!isGameStarted) {
         isGameStarted = true;
-        // Reset the board and shuffle bombs and points
         resetBoard();
-        // Update the displayed robux
         updateRobux();
     }
 }
 
-// Function to reset the game board
 function resetBoard() {
-    // Your code to shuffle bombs and points
+    // Implement the logic to shuffle bombs and points
     // ...
 }
 
-// Function to reveal a square when clicked
 function reveal(square) {
     if (isGameStarted) {
-        // Your code to handle revealing squares
+        // Implement the logic to reveal squares and handle game outcome
         // ...
     }
 }
 
-// Function to update the displayed robux
 function updateRobux() {
-    document.getElementById("robux").textContent = robux;
+    document.getElementById("robux-amount").textContent = robux;
 }
 
-// Implement the logic for betting and winning robux here
-// ...
-
-// Implement the logic for game over, winning, and losing here
+// Implement the logic for betting, winning, and losing robux here
 // ...
